5584774c48e83d46321db78f7164f9b96ca7764f
Vasyl Batig <batig.v@lextools.io>
2024-11-22 13:46:55 +0200

    Fix duplicate, rename folder github action

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,46 +1,46 @@
 import React from 'react';
 
 interface iconsTypes {
-    className?: string
-    onClick: () => void
+  className?: string;
+  onClick: () => void;
 }
 
-export const CloseIcon: React.FC<iconsTypes> = ({className = '', ...props}) => {
-    return <i {...props} className={`icon-close ${className}`} />;
-}
-
-export const VueIcon = ({className = '', ...props}) => {
-  return <i {...props} className={`fab fa-vuejs ${className}`} />;
+export const CloseIcon: React.FC<iconsTypes> = ({ className = '', ...props }) => {
+  return <i { ...props } className={ `icon-close ${ className }` }/>;
 };
 
-export const AmazonIcon = ({className = '', ...props}) => {
-  return <i {...props} className={`fab fa-amazon ${className}`} />;
+export const VueIcon = ({ className = '', ...props }) => {
+  return <i { ...props } className={ `fab fa-vuejs ${ className }` }/>;
 };
 
-export const SassIcon = ({className = '', ...props}) => {
-  return <i {...props} className={`fab fa-sass ${className}`} />;
+export const AmazonIcon = ({ className = '', ...props }) => {
+  return <i { ...props } className={ `fab fa-amazon ${ className }` }/>;
 };
 
-export const FacebookIcon = ({className = '', ...props}) => {
-  return <i {...props} className={`fab fa-facebook-f ${className}`} />;
+export const SassIcon = ({ className = '', ...props }) => {
+  return <i { ...props } className={ `fab fa-sass ${ className }` }/>;
 };
 
-export const TweeterIcon = ({className = '', ...props}) => {
-  return <i {...props} className={`fab fa-twitter ${className}`} />;
+export const FacebookIcon = ({ className = '', ...props }) => {
+  return <i { ...props } className={ `fab fa-facebook-f ${ className }` }/>;
 };
 
-export const TwitchIcon = ({className = '', ...props}) => {
-  return <i {...props} className={`fab fa-twitch ${className}`} />;
+export const TweeterIcon = ({ className = '', ...props }) => {
+  return <i { ...props } className={ `fab fa-twitter ${ className }` }/>;
 };
 
-export const YoutubeIcon = ({className = '', ...props}) => {
-  return <i {...props} className={`fab fa-youtube ${className}`} />;
+export const TwitchIcon = ({ className = '', ...props }) => {
+  return <i { ...props } className={ `fab fa-twitch ${ className }` }/>;
 };
 
-export const SlackIcon = ({className = '', ...props}) => {
-  return <i {...props} className={`fab fa-slack-hash ${className}`} />;
+export const YoutubeIcon = ({ className = '', ...props }) => {
+  return <i { ...props } className={ `fab fa-youtube ${ className }` }/>;
 };
 
-export const ViberIcon = ({className = '', ...props}) => {
-  return <i {...props} className={`fab fa-viber ${className}`} />;
+export const SlackIcon = ({ className = '', ...props }) => {
+  return <i { ...props } className={ `fab fa-slack-hash ${ className }` }/>;
+};
+
+export const ViberIcon = ({ className = '', ...props }) => {
+  return <i { ...props } className={ `fab fa-viber ${ className }` }/>;
 };
