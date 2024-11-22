e4418b8044d53c5ea0f41c93a0a699048037beb9
Vasyl Batig <batig.v@lextools.io>
2024-11-22 16:14:04 +0200

    Fix image path less slash

--- src/utils/generateImgPath.ts
+++ src/utils/generateImgPath.ts
@@ -1,6 +1,6 @@
 const generateImgPath = (path: string): string => {
   const isProd = process.env.NODE_ENV === 'production';
-  return isProd ? `/book/${ path }` : path;
+  return isProd ? `/book${ path }` : path;
 };
 
 export default generateImgPath;
