cca72d5b3bf43e3ab71d348e9c3a562d354a1e76
Vasyl Batig <batig.v@lextools.io>
2024-11-22 14:35:18 +0200

    Try fix build gh pages 4

--- next.config.mjs
+++ next.config.mjs
@@ -4,9 +4,6 @@ const nextConfig = {
   basePath: process.env.NODE_ENV === 'production' ? '/book' : '',
   output: 'export',
   // distDir: './build',
-  images: {
-    unoptimized: true
-  }
 };
 
 export default nextConfig;
