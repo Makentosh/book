4c337f21dfdf92c2a7bf817017185571e2285d5a
Vasyl Batig <batig.v@lextools.io>
2024-12-03 17:55:01 +0200

    Add animation from book content section

--- app/AnimationProvider.tsx
+++ app/AnimationProvider.tsx
@@ -1,8 +1,9 @@
 'use client';
 
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { TextPlugin } from 'gsap/TextPlugin';
 import gsap from 'gsap';
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 
 export const AnimationProvider = ({ children }: { children: any }) => {
 
@@ -28,7 +29,7 @@ export const AnimationProvider = ({ children }: { children: any }) => {
 
   };
 
-  const initScrollAnimations = () => {
+  const initScrollAnimations = useCallback(() => {
     gsap.to('.section-offer-img', {
       scrollTrigger: {
         trigger: '.header',
@@ -109,40 +110,98 @@ export const AnimationProvider = ({ children }: { children: any }) => {
           }
 
         });
-  };
+  }, []);
 
   const initToTopButtonAnimation = () => {
-    gsap.fromTo('.to-top', {
-          scrollTrigger: {
-            trigger: '.header',
-            start: 'top top',
-            scrub: true,
-          },
-          top: 0,
+    const toTopButton = document.querySelector('.to-top');
+
+    // Перевірка стану скролу при завантаженні
+    if ( window.scrollY > 10 ) {
+      gsap.fromTo(
+          toTopButton,
+          { top: 0, opacity: 0 },
+          { top: '92%', opacity: 1, duration: 1, ease: 'power3.inOut' }
+      );
+    } else {
+      gsap.set(toTopButton, { opacity: 0, top: 0 });
+    }
+    //show button
+    gsap.to(toTopButton, {
+      scrollTrigger: {
+        trigger: 'body',
+        start: 'top+=10',
+        toggleActions: 'play none none reverse',
+      },
+      top: '92%',
+      opacity: 1,
+      duration: 1,
+      ease: 'power3.inOut'
+    });
+
+    // hide button if start page
+    gsap.to(toTopButton, {
+      scrollTrigger: {
+        trigger: 'body',
+        start: 'top top',
+        end: 'top+=10',
+        toggleActions: 'reverse none none play',
+      },
+      top: 0,
+      opacity: 0,
+      duration: 1,
+      ease: 'power3.inOut',
+    });
+
+  };
+
+  const initBookContentAnimation = () => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.section-book-content',
+        start: '-50% top',
+      }
+    });
+
+    tl.fromTo('.section-book-content__title', { opacity: 0, x: -200 }, { opacity: 1, x: 0 }, 0.1)
+        .fromTo('.section-book-content__subtitle', { opacity: 0, x: 200 }, { opacity: 1, x: 0 }, 0.2)
+        .fromTo('.about__title', { opacity: 0, y: -100 }, { opacity: 1, y: 0 }, 0.3)
+        .fromTo('.table__title', { opacity: 0, y: 200 }, { opacity: 1, y: 0 }, 0.3)
+        .from('.about__text', { duration: 3.5, text: '' }, 0.4)
+        .fromTo('.social', { opacity: 0, y: 100 }, { opacity: 1, y: 0, delay: 3 }, 0.5)
+        .fromTo('.table-list__item', {
           opacity: 0,
-          duration: 1,
-        },
-        {
-          scrollTrigger: {
-            trigger: '.header',
-            start: 'top top',
-            scrub: true,
-          },
+          x: -100,
+        }, {
           opacity: 1,
-          top: '92%'
-        });
+          stagger: 0.2,
+          ease: 'power3.out',
+          x: 0
+        }, 0.6)
+        .fromTo('.table__btn',
+            { display: 'block',
+              scale: 0,
+              rotate: -180,
+              opacity: 0 },
+            {
+              scale: 1,
+              rotate: 0,
+              opacity: 1,
+              duration: 1,
+              delay: 2,
+              ease: 'elastic.out(1, 0.5)',
+            }, 0.7);
   };
 
 
   useEffect(() => {
     if ( typeof window !== 'undefined' ) {
-      gsap.registerPlugin(ScrollTrigger);
+      gsap.registerPlugin(ScrollTrigger, TextPlugin);
       initHeaderAnimation();
       initScrollAnimations();
-      // initStatisticsAnimation();
       initToTopButtonAnimation();
+      initBookContentAnimation();
     }
-  }, []);
+  }, [initScrollAnimations]);
 
   return children;
 };
